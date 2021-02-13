b4819217e7146170520a869219020af5fd277812
MrPomme <damien.tournie@edu.itescia.fr>
2021-02-13 10:42:00 +0100

    Menu manager

---
+++ Assets/Scripts/Menu Script/MenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static void GoToMenu(MenuName name)
+    {
+        switch(name)
+        {
+            case MenuName.Main:
+
+                SceneManager.LoadScene("MainMenu");
+                break;
+            case MenuName.Pause:
+
+                Object.Instantiate(Resources.Load("PauseMenu"));
+                break;
+        }
+    }
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
