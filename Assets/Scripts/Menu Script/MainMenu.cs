4ad151407a134ffc3967563411c16d3493f23e25
MrPomme <damien.tournie@edu.itescia.fr>
2021-03-15 15:55:18 +0100

    Menu Principal

---
+++ Assets/Scripts/Menu Script/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menuFirstButton, optionsCloseButton, campagneButton, onlineButton, quitButton;
+
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("MusiqueMenu");
+    }
+
+    public void VersusGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+
+}
