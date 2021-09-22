69ebac1ae9415174208c63c95fe2e93e646f5308
MrPomme <damien.tournie@edu.itescia.fr>
2021-09-22 12:30:17 +0200

    refonte menu en cours

--- Assets/Scripts/Menu Script/SplashScreen.cs
+++ Assets/Scripts/Menu Script/SplashScreen.cs
@@ -6,23 +6,33 @@ using UnityEngine.SceneManagement;
 public class SplashScreen : MonoBehaviour
 {
 
+    public GameObject boutton;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(clignotement());
     }
 
     public void OnStart()
     {
         SceneManager.LoadScene("Menu Principal");
     }
+
+    IEnumerator clignotement()
+    {
+        boutton.SetActive(true);
+        yield return new WaitForSeconds(1);
+        boutton.SetActive(false);
+        yield return new WaitForSeconds(1);
+    }
+
 }
