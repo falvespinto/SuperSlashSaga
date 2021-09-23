771fee26900ef2f7c62da4ab17072491eecd3661
MrPomme <damien.tournie@edu.itescia.fr>
2021-09-23 17:09:56 +0200

    Nouveau système de son

---
+++ Assets/Scripts/Musique/OneShotAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OneShotAudio : MonoBehaviour {
+	[SerializeField] private bool playOnEnable = default;
+	[SerializeField] private AudioSource source = default;
+	[SerializeField] private AudioClip[] audioClips = default;
+    [SerializeField] private Vector2 pitchBounds = Vector2.one;
+    
+	private void OnEnable() {
+		if (playOnEnable) {
+			PlayTheSound();
+        }
+    }
+
+    public void PlayTheSound() {
+		if (audioClips.Length > 0) {
+            source.pitch = Random.Range(pitchBounds.x, pitchBounds.y);
+            AudioClip clip = audioClips[Random.Range(0, audioClips.Length)];
+            source?.PlayOneShot(clip);
+		}
+	}
+
+    public void PlayTheSound(float delay)
+    {
+        Invoke("PlayTheSound", delay);
+    }
+}
